08b38cf284ff66667897bac5f73df5be3b649ca5
Charlie Somerville <charlie@charliesomerville.com>
2013-12-26 19:53:14 +1100

    execute config file

--- src/main.c
+++ src/main.c
@@ -1,11 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <luajit-2.0/lua.h>
+#include <luajit-2.0/lauxlib.h>
+#include <luajit-2.0/lualib.h>
+#include <luajit-2.0/luajit.h>
+
+static lua_State*
+init_lua()
+{
+    lua_State* lua = lua_open();
+
+    if(!lua) {
+        fprintf(stderr, "Could not create LuaJIT state\n");
+        exit(1);
+    }
+
+    luaL_openlibs(lua);
+
+    return lua;
+}
 
 int
 main(int argc, char** argv)
 {
-    printf("Hello world!\n");
+    if(argc < 2) {
+        fprintf(stderr, "Usage: orka <config file>\n");
+        exit(1);
+    }
 
-    (void)argc;
-    (void)argv;
+    lua_State* lua = init_lua();
+
+    if(luaL_dofile(lua, argv[1])) {
+        fprintf(stderr, "%s\n", lua_tostring(lua, -1));
+        exit(1);
+    }
+
+    lua_close(lua);
+    return 0;
 }
